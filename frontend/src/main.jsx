3fc018221f1de977a499bc0514f037403fa92e09
Damián Carrera <150828304+Valorianvaelum@users.noreply.github.com>
2026-08-02 10:27:42 -0300

    feat: cargar estilos visuales por módulo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 import "./styles/comfort.css";
 import "./styles/aura-galaxy.css";
 import "./styles/aura-galaxy-polish.css";
+import "./styles/aura-modules.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
